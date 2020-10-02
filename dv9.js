57d8c78d781081dd1ad428ec91559ee6b2d320ab
sgalal <sgalal.me@outlook.com>
2020-10-02 11:45:52 +0800

    Update dv9.js

--- dv9.js
+++ dv9.js
@@ -111,7 +111,7 @@ function 韻母規則() {
 		return !is('入聲') ? 'oun' : 'ak';
 	}
 	if (is('庚韻 合口 二等 或 耕韻')) return !is('入聲') ? 'oun' : 'ak';
-	if (is('庚韻 開口 三等 或 庚韻 合口 三等 或 清青韻')) return !is('入聲') ? 'ein' : 'ek';
+	if (is('庚韻 開口 三等 或 庚韻 合口 三等 或 清青韻')) return !is('入聲') ? 'ein' : 'eg';
 	// 曾攝
 	if (is('登韻')) return !is('入聲') ? 'oun' : 'ok';
 	if (is('蒸韻')) {
@@ -155,11 +155,15 @@ if (is('影母') && 韻母.startsWith('i')) {
 
 function 聲調規則() {
 	if (is('幫滂端透知徹精清心莊初生章昌書見溪影曉母')) {  // 全清、次清
-		if (is('平去入聲')) return 'ˉ';
+		if (is('平聲')) return 'ˉ';
 		if (is('上聲')) return 'ˊ';
+		if (is('去聲')) return '-';
+		if (is('入聲 深臻曾通攝')) return 'ˉ';
+		if (is('入聲')) return '-';
 	} else {
-		if (is('平去入聲')) return 'ˇ';
+		if (is('並定澄從邪崇俟船常羣匣母 上聲')) return 'ˇ';  // 全濁上變去
 		if (is('上聲')) return 'ˊ';
+		if (is('平去入聲')) return 'ˇ';
 	}
 	throw new Error('無聲調規則');
 }
