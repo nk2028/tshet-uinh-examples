7a5998b407cde739d5374621c778a43cff2d2b67
unt <unt_tianjin@163.com>
2020-11-23 20:28:30 +0800

    Split nucleus and coda
    Signed-off-by: unt <unt_tianjin@163.com>

--- unt_2020.js
+++ unt_2020.js
@@ -12,18 +12,20 @@ const is = (x) => 音韻地位.屬於(x);
 const 见系非三等用小舌音 = true;
 const 晓母非三等用小舌音 = true;
 const 知组声母用卷舌塞音 = false; // 打开：ʈ，关闭：tɹ。默认关闭
-const 支鱼虞韵用概念音值 = false; // 包括钟韵
-const 三子韵腹用概念元音 = false;
+const 支鱼虞韵用渐央元音 = false; // 包括钟韵
+const 三子韵核用底层表达 = false;
 const 二等元音用双下横线 = false; // 打开：双下横线 a̳ (U+0333)，关闭：下等号 a͇ (U+0347)。默认关闭，但 Times New Roman 把这两个附加符号弄反了
-const 歌系韵腹用前低元音 = false;
-const 豪韵韵腹用半低元音 = true;
-const 覃韵韵腹用半低元音 = true;
-const 清韵韵腹用次低元音 = true;
-const 庄组臻摄用舌冠韵腹 = true; // 包括合口“率”小韵
-const 侯韵韵腹用二合元音 = true;
+const 歌系韵核用前低元音 = false;
+const 豪韵韵核用半低元音 = true;
+const 覃韵韵核用半低元音 = true;
+const 清韵韵核用次低元音 = true;
+const 庄组臻摄用舌冠韵核 = true; // 包括合口“率”小韵
+const 侯韵韵核用二合元音 = true;
 const 通江宕摄用小舌韵尾 = true;
 const 祭泰夬废用龈后韵尾 = true; // 打开：ɹ；关闭：j
 const 声调符号用五度标记 = true;
+const 声调附加符标韵核上 = true; // 关闭：标在音节前
+
 const 精组高元音省略介音 = true; // “踪”打开：tsʉɔɴ˦˧，关闭：tsɹʉɔɴ˦˧。因为会导致拼写太长，默认打开
 
 function getConsonant() {
@@ -96,80 +98,54 @@ function getMedial() {
 	return is('合口') ? 'ɥ̈' : 'j̈';
 }
 
-function getRhyme() {
-	// 通摄
-	if (is('東韻')) return 'uɴ';
-	if (is('冬韻')) return 'oɴ';
-	if (is('鍾韻')) return 支鱼虞韵用概念音值 ? 'ʉəɴ' : 'ʉɔɴ';
-	// 江摄
-	if (is('江韻')) return 'o͇ɴ';
-	// 止摄
-	if (is('支韻')) return 支鱼虞韵用概念音值 ? 'iə' : 'iɛ';
-	if (is('脂韻')) return 'i';
-	if (is('之韻')) return 'ɨ';
-	if (is('微韻')) return 'ɪi';
-	// 遇摄
-	if (is('魚韻')) return 支鱼虞韵用概念音值 ? 'ɨə' : 'ɨɜ';
-	if (is('虞韻')) return 支鱼虞韵用概念音值 ? 'ʉə' : 'ʉɔ';
-	if (is('模韻')) return 'o';
-	// 蟹摄
-	if (is('齊韻')) return 'ej';
-	if (is('祭廢韻')) return 'ɜɹ';
-	if (is('泰韻')) return 'ɑɹ';
-	if (is('佳韻')) return 'e͇';
-	if (is('皆韻')) return 'e͇j';
-	if (is('夬韻')) return 'a͇ɹ';
-	if (is('咍韻')) return 'əj';
-	if (is('灰韻')) return 'oj';
-	// 臻摄
-	if (is('眞諄臻欣文韻')) return 'ɪn';
-	if (is('元韻')) return 'ɜn';
-	if (is('痕韻')) return 'ən';
-	if (is('魂韻')) return 'on';
-	// 山摄
-	if (is('寒桓韻')) return 'ɑn';
-	if (is('刪韻')) return 'a͇n';
-	if (is('山韻')) return 'e͇n';
-	if (is('先韻')) return 'en';
-	if (is('仙韻')) return 'ɜn';
-	// 效摄
-	if (is('蕭韻')) return 'ew';
-	if (is('宵韻')) return 'ɜw';
-	if (is('肴韻')) return 'e͇w';
-	if (is('豪韻')) return 豪韵韵腹用半低元音 ? 'ʌw' : 'ɑw';
-	// 果摄
-	if (is('歌戈韻')) return 'ɑ';
-	// 假摄
-	if (is('麻韻 二等')) return 'a͇';
-	if (is('麻韻 三等')) return 'a';
-	// 宕摄
-	if (is('陽唐韻')) return 'ɑɴ';
-	// 梗摄
-	if (is('庚韻 二等')) return 'a͇ŋ';
-	if (is('庚韻 三等')) return 'aŋ';
-	if (is('耕韻')) return 'e͇ŋ';
-	if (is('清韻')) return 清韵韵腹用次低元音 ? 'ɐŋ' : 'ɜŋ'; 
-	if (is('青韻')) return 'eŋ';
-	// 曾摄
-	if (is('蒸韻')) return 'iŋ';
-	if (is('登韻')) return 'əŋ';
-	// 流摄
-	if (is('尤韻')) return 'u';
-	if (is('侯韻')) return 侯韵韵腹用二合元音 ? 'ɘu' : 'u';
-	if (is('幽韻')) return 'iu';
-	// 深摄
-	if (is('侵韻')) return 'ɪm';
-	// 咸摄
-	if (is('覃韻')) return 覃韵韵腹用半低元音 ? 'ʌm' : 'əm';
-	if (is('談韻')) return 'ɑm';
-	if (is('鹽嚴凡韻')) return 'ɜm';
-	if (is('添韻')) return 'em';
-	if (is('咸韻')) return 'e͇m';
-	if (is('銜韻')) return 'a͇m';
-	throw new Error('无韵基规则');
+function getCoda() {
+	if (is('通江宕攝') && 通江宕摄用小舌韵尾) return is('入聲') ? 'q' : 'ɴ';
+	if (is('通江宕梗曾攝')) return is('入聲') ? 'k' : 'ŋ';
+	if (is('深咸攝')) return is('入聲') ? 'p' : 'm';
+	if (is('微韻')) return 'i'; // 为了方便，把微韵拆成 i 韵尾
+	if (is('蟹攝')) {
+		if (is('佳韻')) return '';
+		if (is('祭泰夬廢韻') && 祭泰夬废用龈后韵尾) return 'ɹ';
+		return 'j';
+	}
+	if (is('臻山攝')) return is('入聲') ? 't' : 'n';
+	if (is('效攝')) return 'w';
+	return '';
 }
 
-// TODO: split nucleus and coda
+function getVowel() {
+	if (is('脂　蒸　　　韻')) return 'i';
+	if (is('之　　　　　韻')) return 'ɨ';
+	if (is('尤東　　　　韻')) return 'u';
+	if (is('佳耕咸皆山　韻')) return 'e͇';
+	if (is('　青添齊先蕭韻')) return 'e';
+	if (is('　登覃咍痕　韻')) return 'ə';
+	if (is('模冬　灰魂　韻')) return 'o';
+	if (is('麻庚銜夬刪肴韻 二等')) return 'a͇';
+	if (is('麻庚　　　　韻 三等')) return 'a';
+	if (is('歌陽談泰寒豪韻')) return 'ɑ';
+	if (is('戈唐　　桓　韻')) return 'ɑ';
+
+	if (is('江韻')) return 'o͇';
+	if (is('微侵眞諄臻欣文韻')) return 'ɪ';
+	if (is('清鹽嚴凡祭廢仙元宵韻')) return 'ɜ';
+
+	if (is('幽韻')) return 'iu';
+	if (is('侯韻')) return 侯韵韵核用二合元音 ? 'ɘu' : 'u';
+	if (is('支韻')) return 支鱼虞韵用渐央元音 ? 'iə' : 'iɛ';
+	if (is('魚韻')) return 支鱼虞韵用渐央元音 ? 'ɨə' : 'ɨɜ';
+	if (is('虞鍾韻')) return 支鱼虞韵用渐央元音 ? 'ʉə' : 'ʉɔ';
+
+	throw new Error('无元音规则');
+}
+
+function getNucleus() {
+	if (is('清韻') && 清韵韵核用次低元音) return 'ɐ';
+	if (is('覃韻') && 覃韵韵核用半低元音) return 'ʌ';
+	if (is('豪韻') && 豪韵韵核用半低元音) return 'ʌ';
+	if (is('莊組 臻攝') && 庄组臻摄用舌冠韵核) return 'ɹ̩';
+	return getVowel();
+}
 
 function getTone() {
 	const is清 = is('幫滂端透知徹精清心莊初生章昌書見溪影曉母')
@@ -182,66 +158,44 @@ function getTone() {
 		if (is('去聲')) return is清 ? '˥˦˥' : '˧˨˧';
 		if (is('入聲')) return !is全浊 ? '˥' : '˨˩';
 	} else {
-		if (is('平聲')) return is清 ? ' ᷇' : ' ᷅';
-		if (is('上聲')) return !is全浊 ? ' ̋' : ' ̌';
-		if (is('去聲')) return is清 ? ' ̂' : ' ̏';
-		if (is('入聲')) return !is全浊 ? ' ́' : ' ̀';
+		if (is('平聲')) return is清 ? '᷇' : '᷅';
+		if (is('上聲')) return !is全浊 ? '̋' : '̌';
+		if (is('去聲')) return is清 ? '̂' : '̏';
+		if (is('入聲')) return !is全浊 ? '́' : '̀';
 	}
 	throw new Error('无声调规则');
 }
 
 let initial = getInitial();
 let medial = getMedial();
-let rhyme = getRhyme();
+let nucleus = getNucleus();
+let coda = getCoda();
 let tone = getTone();
 
-if (!三子韵腹用概念元音) {
-	if (rhyme.indexOf('ɪ') != -1) {
-		if (is前元音韵) {
-			rhyme = rhyme.replace('ɪ', 'i');
-		} else {
-			rhyme = rhyme.replace('ɪ', is('合口') ? 'ʉ' : 'ɨ');
-		}
-	} else {
-		if (!is('魚韻')) {
-			rhyme = rhyme.replace('ɜ', is前元音韵 ? 'e' : 'ə');
-		}
-		rhyme = rhyme.replace('ɐ', 'æ');
+if (!三子韵核用底层表达) {
+	if (nucleus == 'ɪ') {
+		nucleus = is前元音韵 ? 'i' : is('合口') ? 'ʉ' : 'ɨ';
+	} else if (nucleus == 'ɜ') {
+		nucleus = is前元音韵 ? 'e' : 'ə';
+	} else if (nucleus == 'ɐ') {
+		nucleus = 'æ';
 	}
 }
 
-if (二等元音用双下横线) rhyme = rhyme.replace('͇', '̳');
+if (二等元音用双下横线) nucleus = nucleus.replace('͇', '̳');
 
-if (歌系韵腹用前低元音) rhyme = rhyme.replace('ɑ', 'a');
+if (歌系韵核用前低元音) nucleus = nucleus.replace('ɑ', 'a');
 
-if (庄组臻摄用舌冠韵腹 && is('莊組 臻攝')) rhyme = 'ɹ̩n';
-
-if (!通江宕摄用小舌韵尾) rhyme = rhyme.replace('ɴ', 'ŋ');
-
-if (!祭泰夬废用龈后韵尾) rhyme = rhyme.replace('ɹ', 'j');
-
-if (rhyme.startsWith('i') && (medial == 'j' || medial == 'ɥ')
-	|| rhyme.startsWith('ɨ') && (medial == 'j̈')
-	|| rhyme.startsWith('ʉ') && (medial == 'ɥ̈')) {
+if (nucleus.startsWith('i') && (medial == 'j' || medial == 'ɥ')
+	|| nucleus.startsWith('ɨ') && (medial == 'j̈')
+	|| nucleus.startsWith('ʉ') && (medial == 'ɥ̈')) {
 	medial = '';
 }
 
-if (精组高元音省略介音 && is('精組') && (rhyme.startsWith('ɨ') || rhyme.startsWith('ʉ'))) {
+if (精组高元音省略介音 && is('精組') && (nucleus.startsWith('ɨ') || nucleus.startsWith('ʉ'))) {
 	medial = '';
 }
 
-if (is('入聲')) {
-	if (rhyme.endsWith('m'))
-		rhyme = rhyme.slice(0, -1) + 'p';
-	else if (rhyme.endsWith('n'))
-		rhyme = rhyme.slice(0, -1) + 't';
-	else if (rhyme.endsWith('ŋ'))
-		rhyme = rhyme.slice(0, -1) + 'k';
-	else if (rhyme.endsWith('ɴ'))
-		rhyme = rhyme.slice(0, -1) + 'q';
-}
-
-let result = initial + medial + rhyme;
-result = 声调符号用五度标记 ? result + tone : tone + result;
-
-return result;
+if (声调符号用五度标记) return initial + medial + nucleus + coda + tone;
+if (声调附加符标韵核上) return initial + medial + nucleus + tone + coda;
+return ' ' + tone + initial + medial + nucleus + coda;
