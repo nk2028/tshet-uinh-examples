5770cd911c9521da085f680908f5c4b57b60479e
uliloewi <zequanyu@yahoo.de>
2021-08-01 11:54:25 +0200

    對映-〉對應

--- langjin.js
+++ langjin.js
@@ -2,8 +2,8 @@
  *
  * https://zhuanlan.zhihu.com/p/391166351
  *
- * 中古音與南京音的對映表：https://github.com/uliloewi/lang2jin1/blob/master/Guangyun_Langjin_pulish_Alphabetic.2.0.csv
- * 南京音本是清末以前標準官話的基礎音系，和中古音有嚴格的對映關系。故有上表。本程序展示此對映關系。
+ * 中古音與南京音的對應表：https://github.com/uliloewi/lang2jin1/blob/master/Guangyun_Langjin_pulish_Alphabetic.2.0.csv
+ * 南京音本是清末以前標準官話的基礎音系，和中古音有嚴格的對應關系。故有上表。本程序展示此對應關系。
  * 南京話拼音方案：https://zh.wikipedia.org/wiki/%E5%8D%97%E4%BA%AC%E8%A9%B1%E6%8B%89%E4%B8%81%E5%8C%96%E6%96%B9%E6%A1%88#%E8%BC%B8%E5%85%A5%E6%B3%95%E6%96%B9%E6%A1%88
  *
  * @author uliloewi
