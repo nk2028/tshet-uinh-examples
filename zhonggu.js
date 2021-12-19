7d4efa3c614f4524e25420ca1155078e440ee143
uliloewi <zequanyu@yahoo.de>
2021-12-19 20:21:12 +0100

    Update zhonggu.js
    修改

--- zhonggu.js
+++ zhonggu.js
@@ -92,16 +92,16 @@ function 韻母規則() {
   if (is('灰韻')) return 'uai';//皆一合
   if (is('廢韻')) return is('合口') ? 'ʉæɿ' : 'ɨæɿ';//皆三等
   // 臻攝 
-  if (is('眞韻 合口')) return is('重紐A類') ? 'yən' : 'ʉən';//皆三合
-  if (is('眞韻') && is('幫組 或 開口')) return is('重紐A類') ? 'iən' : 'ɨən';//皆三開
+  if (is('眞韻 合口')) return is('重紐A類') ? 'yn' : 'ʉn';//皆三合
+  if (is('眞韻') && is('幫組 或 開口')) return is('重紐A類') ? 'in' : 'ɨn';//皆三開
   if (is('臻韻')) return 'ɨən';//皆莊組三開
   if (is('欣韻')) return 'ɨon';//皆牙喉音三開
-  if (is('文韻')) return 'ʉon';//皆三合
+  if (is('文韻')) return 'ʉən';//皆三合
   // if (is('元韻 開口')) return 'ian';
   // if (is('元韻')) return 'yan';
   if (is('痕韻')) return 'on';//皆一開
   if (is('魂韻')) return 'uon';//皆一合
-  if (is('諄韻')) return 'ʉən';//皆三合 即眞B合
+  if (is('諄韻')) return 'ʉn';//皆三合 即眞B合
   // 山攝 
   if (is('寒桓韻')) return is('三等') ? 'ɨan' : is('合口') ? 'uan' : 'an';//皆一開，除了䔾三開
   if (is('刪韻')) return is('合口') ? 'ʯan' : 'ʅan';//皆二等
@@ -109,7 +109,7 @@ function 韻母規則() {
   if (is('元韻')) return is('合口') ? 'ʉan' : 'ɨan';//皆三等
   if (is('仙韻 合口')) return is('重紐A類') ? 'yæn' : 'ʉæn';//皆三等
   if (is('仙韻') && is('幫組 或 開口')) return is('重紐A類') ? 'iæn' : 'ɨæn';//皆三等
-  if (is('先韻')) return is('合口') ? 'yan' : 'ian';//皆四等
+  if (is('先韻')) return is('合口') ? 'yɛn' : 'iɛn';//皆四等
   // 效攝 
   if (is('蕭韻')) return 'iæu';//皆四開
   if (is('宵韻')) return is('重紐A類') ? 'iau' : 'ɨau';//皆三開
@@ -131,7 +131,7 @@ function 韻母規則() {
   if (is('清韻')) return is('合口') ?  'yæŋ' : 'iæŋ';//皆三等A類
   if (is('青韻')) return is('合口') ? 'yɛŋ' : 'iɛŋ';//皆四等
   // 曾攝 
-  if (is('蒸韻')) return is('合口') ? 'ʉəŋ' : 'ɨəŋ';//皆三等
+  if (is('蒸韻')) return is('合口') ? 'ʉŋ' : 'ɨŋ';//皆三等
   if (is('登韻')) return is('合口') ? 'uəŋ' : 'əŋ';//皆一等
   // 流攝 
   if (is('尤韻')) return 'ɨu';//皆三開
