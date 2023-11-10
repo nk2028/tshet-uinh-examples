14ce9b41d86bf3d4a5312ae23af81aedb80e6001
untunt <unt_tianjin@163.com>
2023-11-10 22:28:03 +0800

    Update chiangxhua and zhongyuan Update chiangxhua: write medials as semivowels; ɨ > ə Update zhongyuan: add `齊微韻洪音韻尾`

--- test/main.js
+++ test/main.js
@@ -47,7 +47,7 @@ assert_equal(panwuyun(音韻地位), "ɕiᴇu˧˥");
 assert_equal(unt(音韻地位), "ɕéw");
 assert_equal(msoeg_v8(音韻地位), "çiɛuˀ");
 assert_equal(mid_tang(音韻地位), "ɕiɛw˦˥");
-assert_equal(chiangxhua(音韻地位), "ʃiɛ́w");
+assert_equal(chiangxhua(音韻地位), "ɕjɛ́w");
 assert_equal(mongol(音韻地位), "ꡮꡠꡓ");
 assert_equal(zhongyuan(音韻地位), "ʂjɛw³");
 assert_equal(fanwan(音韻地位), "shiu2");
