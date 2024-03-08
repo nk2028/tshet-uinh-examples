7001c85d150a84566c3ea6310fa6b492a2314109
Estela ad Astra <i@estela.moe>
2024-03-08 03:30:37 +0100

    Add Extrapolated Piau-ciun Ngu-ngei (Standard Wu) (#54)

--- README.md
+++ README.md
@@ -39,6 +39,7 @@ https://nk2028-1305783649.file.myqcloud.com/qieyun-examples/<filename>
 - 推導上海話 (Extrapolated Shanghainese): `zaonhe.js`
 - 推導南京話 (Extrapolated Nankinese): `langjin.js`
 - 推導大埔話 (Extrapolated Taibu Hakka): `taibu.js`
+- 推導標準吳語 (Extrapolated Standard Wu): `ngungei.js`
 
 **人造音系 artificial phonological system**
 
--- test/main.js
+++ test/main.js
@@ -22,6 +22,7 @@ import {
   zaonhe,
   langjin,
   taibu,
+  ngungei,
   ayaka_v8,
   yec_en_hua,
 } from "../index.js";
@@ -56,6 +57,7 @@ assert_equal(gwongzau(音韻地位), "siu2");
 assert_equal(zaonhe(音韻地位), "sɔ̄");
 assert_equal(langjin(音韻地位), "shao³");
 assert_equal(taibu(音韻地位), "shau3");
+assert_equal(ngungei(音韻地位), "shiau2");
 assert_equal(ayaka_v8(音韻地位), "seu");
 assert_equal(yec_en_hua(音韻地位), "A");
 
