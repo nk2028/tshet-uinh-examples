0bc36791ce1d466de09746ce3eef3f021e4eabd6
uliloewi <zequanyu@yahoo.de>
2024-11-19 15:32:25 +0100

    測試

--- test/main.js
+++ test/main.js
@@ -21,7 +21,7 @@ const testCases = [
   ["putonghua", "shǎo"],
   ["gwongzau", "siu2"],
   ["zaonhe", "sɔ̄"],
-  ["langjin", "shao3"],
+  ["langjin", "shao³"],
   ["ayaka_v8", "seu"],
   ["yec_en_hua", "A"],
 ];
